eb763f9275b3114f7643349440d50d8a74d0a475
Ryan Shofner <ryan@letsgolearn.com>
2016-10-23 13:36:54 -0700

    secure routes

--- src/routes.php
+++ src/routes.php
@@ -1,18 +1,19 @@
 <?php
 
-
+Route::group(['middleware' => 'auth'], function () {
     //GET
 
     Route::get('admin/settings/{module}', [
-        'as' => 'get.admin.settings.index',
-        'uses' => 'AdminSettingsController@getSystem',
-        'middleware' => 'allow.only',
+        'as'          => 'get.admin.settings.index',
+        'uses'        => 'AdminSettingsController@getSystem',
+        'middleware'  => 'allow.only',
         'permissions' => ['settings.edit'],
     ]);
 
     Route::post('admin/settings/save/{module}', [
-        'as' => 'post.admin.settings.save',
-        'uses' => 'AdminSettingsController@postSave',
-        'middleware' => 'allow.only',
+        'as'          => 'post.admin.settings.save',
+        'uses'        => 'AdminSettingsController@postSave',
+        'middleware'  => 'allow.only',
         'permissions' => ['settings.edit'],
     ]);
+});
